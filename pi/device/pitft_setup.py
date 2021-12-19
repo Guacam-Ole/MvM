a1ca85adce760b2f1aff667a10fe1f5885600f06
Ole <git@oles.cloud>
2021-12-19 18:05:49 +0100

    converted python2 to python3

--- pi/device/pitft_setup.py
+++ pi/device/pitft_setup.py
@@ -4,7 +4,7 @@ import time
 import os
 
 def edit_line_file(filename, old_line, new_line):
-	print "    Editing", filename
+	print("    Editing", filename)
 	time.sleep(1)
 	file = open(filename, "r")
 	lines = []
@@ -15,42 +15,42 @@ def edit_line_file(filename, old_line, new_line):
 	for i in range(0, len(lines)):
 		#print lines[i]
 		if old_line in lines[i]:
-			print '    Found "' + old_line + '":      ', lines[i],
+			print('    Found "' + old_line + '":      ', lines[i], end=' ')
 			time.sleep(0.5)
 			if lines[i].strip('\n') == new_line:
-				print "    Already set, skiping..."
+				print("    Already set, skiping...")
 				time.sleep(1)
 				return 0
-			print '    Change to', new_line
+			print('    Change to', new_line)
 			time.sleep(0.5)
 			lines[i] = new_line
 			done = True
 
-	print "    Search finished."
+	print("    Search finished.")
 
 	if not done:
-		print "    Could not find", old_line
+		print("    Could not find", old_line)
 		time.sleep(1)
-		print "    Adding", new_line, "automaticly"
+		print("    Adding", new_line, "automaticly")
 		time.sleep(1)
 		lines.append('\n'+new_line+'\n')
-		print "    Finished."
+		print("    Finished.")
 		time.sleep(1)
-	print "    Preparing new file"
+	print("    Preparing new file")
 	new_file = "".join(lines)
 
 	input_incorrect = True
 	while input_incorrect:
 		message = "    Are you sure to change the file(%s) with new line? (y/n): " % filename
-		value = raw_input(message)
+		value = input(message)
 		if value in ["n", "N"]:
-			print "    The screen will not work, but OK."
+			print("    The screen will not work, but OK.")
 			input_incorrect = False
 			time.sleep(1)
-			print "\n    And I assume you don't wanna go any further, so Bye~"
+			print("\n    And I assume you don't wanna go any further, so Bye~")
 			quit()
 		elif value in ["y", "Y"]:
-			print "    Yes, it is!"
+			print("    Yes, it is!")
 			time.sleep(1)
 			'''
 			print "    Backup..."
@@ -58,19 +58,19 @@ def edit_line_file(filename, old_line, new_line):
 			os.system(cmd)
 			time.sleep(1)
 			'''
-			print "    Write file..."
+			print("    Write file...")
 			file = open(filename, "w")
 			file.write(new_file)
 			file.close()
-			print "   ", filename, "modified to:"
+			print("   ", filename, "modified to:")
 			cmd = "cat " + filename
 			os.system(cmd)
 			input_incorrect = False
 		else:
-			print '    Input Error, please input "y", or "n". Try again or Ctrl + C to Exit.'
+			print('    Input Error, please input "y", or "n". Try again or Ctrl + C to Exit.')
 
 def edit_cmdline():
-	print "    Editing /boot/cmdline.txt"
+	print("    Editing /boot/cmdline.txt")
 	time.sleep(1)
 	file = open("/boot/cmdline.txt", "r")
 	line = file.read()
@@ -80,114 +80,114 @@ def edit_cmdline():
 	blocks = line.split(" ")
 
 	for i in range(0, len(blocks)):
-		print blocks[i]
+		print(blocks[i])
 		if "fbcon=map:" in blocks[i]:
-			print "    Found fbcon=map:    ", blocks[i]
+			print("    Found fbcon=map:    ", blocks[i])
 			time.sleep(0.5)
 			if blocks[i] == "fbcon=map:10":
-				print "    Already set, skiping..."
+				print("    Already set, skiping...")
 				time.sleep(1)
 				return 0
-			print '    Change to "fbcon=map:10"'
+			print('    Change to "fbcon=map:10"')
 			time.sleep(0.5)
 			blocks[i] = "fbcon=map:10"
 			map_done = True
 
 		if "fbcon=font:" in blocks[i]:
-			print "    Found fbcon=font:    ", blocks[i]
+			print("    Found fbcon=font:    ", blocks[i])
 			time.sleep(0.5)
 			if blocks[i] == "fbcon=font:VGA8x8":
-				print "    Already set, skiping..."
+				print("    Already set, skiping...")
 				time.sleep(1)
 				return 0
-			print '    Change to "fbcon=font:VGA8x8"'
+			print('    Change to "fbcon=font:VGA8x8"')
 			time.sleep(0.5)
 			blocks[i] = "fbcon=font:VGA8x8"
 			font_done = True
 
-	print "    Search finished."
+	print("    Search finished.")
 	if not map_done:
-		print "    Could not find fbocn map settings."
+		print("    Could not find fbocn map settings.")
 		time.sleep(1)
-		print "    Adding fbcon=map:10 automaticly"
+		print("    Adding fbcon=map:10 automaticly")
 		time.sleep(1)
 		blocks[-1] = blocks[-1].strip('\n')
 		blocks.append("fbcon=map:10")
-		print "    Finished."
+		print("    Finished.")
 		time.sleep(1)
 	if not font_done:
-		print "    Could not find fbocn font settings."
+		print("    Could not find fbocn font settings.")
 		time.sleep(1)
-		print "    Adding fbcon=font:VGA8x8 automaticly"
+		print("    Adding fbcon=font:VGA8x8 automaticly")
 		time.sleep(1)
 		blocks[-1] = blocks[-1].strip('\n')
 		blocks.append("fbcon=font:VGA8x8")
-		print "    Finished."
+		print("    Finished.")
 		time.sleep(1)
 
-	print "\n    Old line:"
-	print "     ", line
+	print("\n    Old line:")
+	print("     ", line)
 	time.sleep(1)
-	print "\n    New line:"
+	print("\n    New line:")
 	new_line = " ".join(blocks)
-	print "     ", new_line
+	print("     ", new_line)
 	time.sleep(1)
 
 	input_incorrect = True
 	while input_incorrect:
-		value = raw_input("    Are you sure to change the file with new line? (y/n): ")
+		value = input("    Are you sure to change the file with new line? (y/n): ")
 		if value in ["n", "N"]:
-			print "    The screen will not work, but OK."
+			print("    The screen will not work, but OK.")
 			input_incorrect = False
 			time.sleep(1)
-			print "\n    And I assume you don't wanna go any further, so Bye~"
+			print("\n    And I assume you don't wanna go any further, so Bye~")
 			quit()
 		elif value in ["y", "Y"]:
-			print "    Yes, it is!"
+			print("    Yes, it is!")
 			time.sleep(1)
 			file = open("/boot/cmdline.txt", "w")
 			line = file.write(new_line)
 			file.close()
-			print "    /boot/cmdline.txt modified to:"
+			print("    /boot/cmdline.txt modified to:")
 			os.system("cat /boot/cmdline.txt")
 			input_incorrect = False
 		else:
-			print '    Input Error, please input "y", or "n". Try again or Ctrl + C to Exit.'
+			print('    Input Error, please input "y", or "n". Try again or Ctrl + C to Exit.')
 
 def main():
-	print '==========================================='
-	print ""
-	print "         2.2 Inch TFT Screen Setup  "
-	print ""
-	print "                SunFounder"
-	print ""
-	print "                    support@sunfounder.com"
-	print "==========================================="
+	print('===========================================')
+	print("")
+	print("         2.2 Inch TFT Screen Setup  ")
+	print("")
+	print("                SunFounder")
+	print("")
+	print("                    support@sunfounder.com")
+	print("===========================================")
 
-	print "Setting up for PiTFT..."
+	print("Setting up for PiTFT...")
 	time.sleep(2)
 
 
-	print "\n\nEnable I2C and SPI:"
+	print("\n\nEnable I2C and SPI:")
 	time.sleep(1)
 
 	edit_line_file('/boot/config.txt', 'dtparam=i2c_arm=', 'dtparam=i2c_arm=on')
-	print "    I2C enabled."
+	print("    I2C enabled.")
 	time.sleep(1)
 
 	edit_line_file('/boot/config.txt', 'dtparam=spi=', "dtparam=spi=on")
-	print "    SPI enabled."
+	print("    SPI enabled.")
 	time.sleep(1)
 
-	print ""
+	print("")
 
 	time.sleep(3)
 
-	print "\n\nSet up screen:"
+	print("\n\nSet up screen:")
 	time.sleep(2)
 	input_incorrect = True
 	while input_incorrect:
-		rotation = raw_input("    Input the rotation of the screen (0, 90, 180, 270, 360 or leave empty for default setting 270): ")
+		rotation = input("    Input the rotation of the screen (0, 90, 180, 270, 360 or leave empty for default setting 270): ")
 		if rotation == "":
 			input_incorrect = False
 			rotation = 270
@@ -195,7 +195,7 @@ def main():
 			input_incorrect = False
 			rotation = int(rotation)
 		else:
-			print "    Input Error, rotation value must be 0, 90, 180 or 270. Try again or Ctrl + C to Exit."
+			print("    Input Error, rotation value must be 0, 90, 180 or 270. Try again or Ctrl + C to Exit.")
 
 	line = 'dtoverlay=pitft28-resistive, rotate=%d, speed=48000000, fps=30' % rotation
 	edit_line_file('/boot/config.txt', 'dtoverlay=pitft28-resistive', line)
@@ -206,7 +206,7 @@ def main():
 	time.sleep(3)
 
 	# 3
-	print "\n\nAdd devide"
+	print("\n\nAdd devide")
 	time.sleep(2)
 	os.system("mv /usr/share/X11/xorg.conf.d/99-fbturbo.conf /home/pi")
 	edit_line_file("/home/pi/.profile", "export FRAMEBUFFER=", "export FRAMEBUFFER=/dev/fb1")
@@ -219,18 +219,18 @@ def main():
 
 	time.sleep(3)
 	# 4
-	print "\n\nSet auto start X GUI"
+	print("\n\nSet auto start X GUI")
 	time.sleep(2)
 	edit_line_file("/boot/config.txt", "start_x=", "start_x=1")
 
 
-	print "===================="
-	print ""
-	print "      Finished!"
-	print ""
-	print "===================="
+	print("====================")
+	print("")
+	print("      Finished!")
+	print("")
+	print("====================")
 	time.sleep(1)
-	print "\nReboot.."
+	print("\nReboot..")
 	time.sleep(2)
 	os.system("sudo reboot")
 
